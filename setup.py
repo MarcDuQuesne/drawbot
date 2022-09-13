34e9148ded44c6f2096c98aafc8e41e59925c06f
Matteo Giani <matteo.giani@dsm.com>
2022-09-13 14:41:46 +0200

    setup.py and requirements.

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+def requirements(reqs_file="requirements.txt"):
+    with open(reqs_file) as f:
+        required = f.read().splitlines()
+    return [line for line in required if not line.startswith("#")]
+
+
+setup(
+    name="crayion",
+    author="Matteo Giani",
+    author_email="matteo.giani.87@gmail.com",
+    version="0.0.1",
+    install_requires=requirements(),
+    tests_require=requirements("requirements_tests.txt"),
+    extras_require={
+        "dev": requirements("requirements_dev.txt"),
+        "test": requirements("requirements_tests.txt"),
+    },
+    packages=find_packages(exclude=["notebooks", "tests"]),
+    entry_points={
+        "console_scripts": [
+            "crayion = craiyon.craiyon_api:call_api",
+        ],
+    },
+    package_data={"craiyon": ["images/*"]},
+)
